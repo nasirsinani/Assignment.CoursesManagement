58b5f5cd3429f182f10d56cf826d3c284d7437cc
nasirsinani <nasir@orangemelons.com>
2021-09-18 19:45:22 +0200

    Fixed CoursesDbContext.
    Accidentally removed DbSets after migration.

--- AssignmentCoursesManagement.Infrastructure/CoursesDbContext.cs
+++ AssignmentCoursesManagement.Infrastructure/CoursesDbContext.cs
@@ -11,7 +11,8 @@ namespace AssignmentCoursesManagement.Infrastructure
     public class CoursesDbContext : DbContext 
     {
         public CoursesDbContext(DbContextOptions<CoursesDbContext> options) : base(options) {}
-
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CourseStudent>().HasKey(cs => new { cs.CourseId, cs.StudentId });
